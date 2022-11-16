9c061cf6fc482851b6169e88bec32f3555df2db7
MattSperry <matthewjoelsperry@gmail.com>
2022-11-16 14:05:41 -0700

    updateViews

--- Ideas/views.py
+++ Ideas/views.py
@@ -4,9 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def indexPageView(request):
     return render(request, 'ideas/index.html')
+
 def aboutPageView(request):
     return render(request, 'ideas/about.html')
+
 def uploadPageView(request):
-    return HttpResponse('Upload new ideas here!')
+    return render(request, 'ideas/uploads.html')
+
 def ideasPageView(request, fName, lName):
-    return HttpResponse(f'<h2>Welcome {fName} {lName}!</h2>') # page for after the user logs in
+    return render(request, 'ideas/ideas.html') # page for after the user logs in
