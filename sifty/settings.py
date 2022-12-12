ab64a1b6bc8bf02fceec74b9c6ef4df88690a29c
MattSperry <matthewjoelsperry@gmail.com>
2022-12-12 15:00:06 -0700

    Co-authored-by: DerekDude77 <DerekDude77@users.noreply.github.com> Co-authored-by: treklewis <treklewis@users.noreply.github.com>

--- sifty/settings.py
+++ sifty/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 from dotenv import load_dotenv
@@ -78,14 +79,10 @@ WSGI_APPLICATION = 'sifty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sifty',
-        'USER': 'postgres',
-        'PASSWORD': 'Flapjack1',
-        'HOST': 'localhost',
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
