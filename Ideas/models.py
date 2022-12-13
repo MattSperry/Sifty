0771d0f93502a724383592092313092d0cdbe950
MattSperry <matthewjoelsperry@gmail.com>
2022-12-12 17:18:33 -0700

    bug fixing

--- Ideas/models.py
+++ Ideas/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from datetime import datetime, timedelta
 
-class IdeaCategory(models.Model):
-    name = models.CharField(max_length=30)
-    def __str__(self):
-        return(self.name)
-
-    class Meta:
-        db_table = 'IdeaCategory'
-
-# two 0..* tables, postgres automatically makes association table
-
 class Customer(models.Model):
     personID = models.OneToOneField('auth.User', on_delete=models.CASCADE, primary_key=True, default=1)
     first_name = models.CharField(max_length=30)
@@ -31,10 +21,20 @@ class Customer(models.Model):
 
     class Meta:
         db_table = 'Customer'
+
+class IdeaCategory(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return(self.name)
+
+    class Meta:
+        db_table = 'IdeaCategory'
+
+# two 0..* tables, postgres automatically makes association table
         
 
 class Idea(models.Model):
-    idea_id = models.AutoField(primary_key=True)
+    idea_id = models.AutoField(primary_key=True, default=1)
     customer = models.ForeignKey('Customer', null=True, blank=True, on_delete=models.SET_NULL)
     category = models.ManyToManyField(IdeaCategory)
     name = models.CharField(max_length = 30)
