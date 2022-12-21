e9f027804f32f0cc58c6fb1c92faffc76def12c0
MattSperry <matthewjoelsperry@gmail.com>
2022-12-20 20:10:09 -0700

    fixing typo

--- sifty/settings.py
+++ sifty/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'sifty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
+# DATABASE_URL = os.getenv("DATABASE_URL")
 
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'sifty',
-#         'USER': 'postgres',
-#         'PASSWORD': 'Flapjack1',
-#         'HOST': 'localhost',
-#     }
-
+#     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sifty',
+        'USER': 'postgres',
+        'PASSWORD': 'Flapjack1',
+        'HOST': 'localhost',
+    }
+
+}
 
 
 # Password validation
